f271012751ff4f5eb7843e9b62118cedb4e6848e
Blair <abrgroup@gmail.com>
2019-12-18 21:16:33 -0500

    cleaned up code

--- src/timer.js
+++ src/timer.js
@@ -38,19 +38,6 @@ const Timer = () => {
         Reset
       </button>
     </div>
-    // <div className="app">
-    //   <div className="time">
-    //     {seconds}s
-    //   </div>
-    //   <div className="row">
-    //     <button className={`button button-primary button-primary-${isActive ? 'active' : 'inactive'}`} onClick={toggle}>
-    //       {isActive ? 'Pause' : 'Start'}
-    //     </button>
-    //     <button className="button" onClick={reset}>
-    //       Reset
-    //     </button>
-    //   </div>
-    // </div>
   );
 };
 
